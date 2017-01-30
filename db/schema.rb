acc3d323087b5b6b086be07a57a677dabac75b32
Basil Khan <basilkhan.dev@gmail.com>
2017-01-30 18:59:59 -0500

    feat(Comments): Generate Comments Scaffold and Include Footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222035543) do
+ActiveRecord::Schema.define(version: 20170130234231) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
