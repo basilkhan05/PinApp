ada384f7fbf71c2d51dcf61fd99f2e7b16bdf43a
Basil Khan <basilkhan.dev@gmail.com>
2017-02-06 20:30:59 -0500

    fix(Rollback): Comments Scaffold rollback

--- app/models/comment.rb
+++
@@ -1,9 +0,0 @@
-class Comment < ActiveRecord::Base
-
-	belongs_to :user
-	belongs_to :pin
-
-	validates :comment, presence: true
-
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131010856) do
+ActiveRecord::Schema.define(version: 20170131010625) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "pin_id"
   end
 
-  add_index "comments", ["pin_id"], name: "index_comments_on_pin_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "pins", force: :cascade do |t|
